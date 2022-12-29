7461b205078f7a5e5ec43af59cab079991fb4650
Ahmed Elfarra <ahmed.e.alfarra@gmail.com>
2022-12-29 22:57:10 +0300

    Rating Component

---
+++ frontend/src/components/Rating.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+const Rating = ({ value, text, color }) => {
+  return (
+    <div className='rating'>
+      {new Array(5)
+        .fill('')
+        .map((_, i) => i + 1)
+        .map((currentStar) => (
+          <span key={currentStar}>
+            <i
+              style={{
+                color,
+              }}
+              className={
+                value >= currentStar
+                  ? 'fas fa-star'
+                  : value >= currentStar - 0.5
+                  ? 'fas fa-star-half-alt'
+                  : 'far fa-star'
+              }
+            ></i>
+          </span>
+        ))}
+      <span>{text && text}</span>
+    </div>
+  )
+}
+
+Rating.defaultProps = {
+  color: '#f8e825',
+}
+
+Rating.propTypes = {
+  value: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string,
+}
+
+export default Rating
